54d3927cfcee9f8a976c866ef531e5606df7790f
khanhnguyen15 <caokhanh98.nguyen@gmail.com>
2018-03-14 13:54:53 +0000

    main menu

---
+++ Jewel Mining game/Assets/Scripts/SwapImage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SwapImage: MonoBehaviour
+{
+
+    public Texture texture1;
+    public Texture texture2;
+    public RawImage img;
+
+    public void swap()
+    {
+        img.texture = (Texture)texture2;
+    }
+
+    public void UnSwap()
+    {
+        img.texture = (Texture)texture1;
+    }
+
+}
---
+++ Jewel Mining game/Assets/Scripts/VolumeSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class VolumeSetting : MonoBehaviour {
+
+    public SwapImage instance;
+    public AudioMixer audioMixer;
+
+	public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        if (volume == -60)
+        {
+            instance.swap();
+        }
+        else
+        {
+            instance.UnSwap();
+        }
+    }
+
+}
