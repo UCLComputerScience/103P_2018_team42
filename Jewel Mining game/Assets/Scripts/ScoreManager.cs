14727a59f6acdb6a434f61cbd44277f4a19a7ab9
Mostafa Ibrahim <32401731+mostafaibrahim17@users.noreply.github.com>
2018-03-19 12:09:26 +0000

    NEW

---
+++ Jewel Mining game/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // unity library for UI
+
+public class ScoreManager : MonoBehaviour {
+
+	public int score = 0;		// the actual score
+	public int targetScore = 400;
+	public Text scoreText;
+	public GameObject youWon;
+	public GameObject gameOver;
+
+
+	void Awake () 
+	{
+		scoreText.text = ("Score: " + score + "/" + targetScore); // initializing scoreText
+
+		//InvokeRepeating("Clock", 0, clockSpeed);// InvokeRepeating allows us to repeatedly call a function , the 2nd parameter is the delay before first call
+		//3rd parameter is the time between each call
+	}
+		
+
+	public void AddPoints(int pointScored)
+	{
+		score += pointScored;
+		scoreText.text = ("Score: " + score + "/" + targetScore);
+	}
+
+	void CheckGameOver()
+	{
+		if (score >= targetScore)
+		{
+			Time.timeScale = 0;
+			youWon.SetActive(true);
+		}
+		else
+		{
+			Time.timeScale = 0;
+			gameOver.SetActive(true);
+		}
+	}
+
+	void Update()
+	{
+		if (score >= targetScore)
+		{
+			Time.timeScale = 0;
+			youWon.SetActive(true);
+		}
+	}
+
+
+}
