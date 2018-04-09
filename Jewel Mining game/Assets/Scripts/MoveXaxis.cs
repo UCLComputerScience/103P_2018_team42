549719053f768ad60291d20eae8eb9b07d6f97dd
Mostafa Ibrahim <32401731+mostafaibrahim17@users.noreply.github.com>
2018-04-09 16:31:21 +0200

    9 April

--- Jewel Mining game/Assets/Scripts/MoveXaxis.cs
+++ Jewel Mining game/Assets/Scripts/MoveXaxis.cs
@@ -3,9 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveXaxis : MonoBehaviour {
-	
+
+	Vector3 position;
+	Vector3 posX;
+	Quaternion rot = new Quaternion (0, 0, 90, 0);
+
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (0, 0, 0.01f);
+		posX.x = -12;
+
+		position = transform.position;
+		//print (position.x);
+		if (position.x > 11 && position.x < 12) {
+			transform.SetPositionAndRotation(posX,transform.rotation);
+		}
+
+		transform.Translate (0, 0, -0.03f);
+
 	}
 }
---
+++ Jewel Mining game/Assets/Scripts/screenWrap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class screenWrap : MonoBehaviour {
+
+	public GameObject obj;
+	public Transform tpLoc;
+
+
+	void OnTriggerEnter(Collider other)
+	{
+		
+		if (other.gameObject.CompareTag("Jewel")) {
+			print ("dd");
+			obj.transform.position = tpLoc.transform.position;
+		}
+	}
+}
