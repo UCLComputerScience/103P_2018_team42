291e86cdb6efdbde0645caab6bcecc40aad23b87
khanhnguyen15 <caokhanh98.nguyen@gmail.com>
2018-04-11 16:42:29 +0100

    adding levels

---
+++ Jewel Mining game/Assets/Scripts/LevelMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelMenu : MonoBehaviour {
+
+	public void SelectEasy () {
+        SceneManager.LoadScene(2);
+    }
+
+    public void SelectNormal()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void SelectHard()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+}
---
+++ Jewel Mining game/Assets/Scripts/PermanentAudio.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PermanentAudio : MonoBehaviour {
+
+	void Awake () {
+        GameObject[] audio = GameObject.FindGameObjectsWithTag("Audio");
+        if (audio.Length > 1)
+            Destroy(this.gameObject);
+        DontDestroyOnLoad(this.gameObject);
+	}
+	
+}
